77589556be64406c4c6308fe164698affb97524c
MichaelKloepzig <mail@michael-kloepzig.de>
2016-07-04 14:41:57 +0200

    typos

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')(),
+	browserSync = require('browser-sync').create(),
+	reload = browserSync.reload,
+	rootPath = './'
+;
+
+
+// LESS
+gulp.task('less', function() {
+	return gulp.src(rootPath + 'rastr.less')
+		.pipe(plugins.less({
+			strictMath: true
+		}))
+		.pipe(plugins.autoprefixer('> 0.5%', 'last 2 versions', 'ie 10'))
+		.pipe(plugins.cssnano({ safe: false, calc: false }))
+		.pipe(plugins.replace(/\.col-([^\}]*)\{-webkit-box-flex:0\}/g, '')) // remove redundant rules
+		.pipe(plugins.replace(/-webkit-box-flex:0;/g, '')) // remove redundant rules
+		.pipe(gulp.dest(rootPath))
+		.pipe(reload({ stream: true }))
+	;
+});
+
+
+// HTML
+gulp.task('html', function() {
+	reload();
+});
+
+
+// WATCH
+gulp.task('watch', function() {
+	browserSync.init({
+		server: rootPath,
+		notify: false,
+		open: false
+	});
+
+	gulp.watch([rootPath + '*.html', rootPath + 'demo.css'], ['html']);
+	gulp.watch([rootPath + '*.less'], ['less']);
+});
+
+
+// DEFAULT
+gulp.task('default', ['watch']);
